05ef7e06f1dbb3551009825fc04fec0f3da4624e
CapsAdmin <eliashogstvedt@gmail.com>
2022-08-27 23:55:54 +0200

    get particles working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { GPU } from "gpu.js";
@@ -6,51 +6,96 @@ import { GPU } from "gpu.js";
 const useGPU = (canvas: HTMLCanvasElement | null) => {
   useEffect(() => {
     if (!canvas) return;
-    const gpu = new GPU({ canvas: canvas, mode: "gpu" });
+    const gpu = new GPU({ canvas: canvas, mode: "webgl2" });
 
-    const dim = 512;
+    const particleCount = 50;
+    const width = 512;
+    const height = 512;
 
-    let runSimulation = gpu.createKernel(
-      function (m: any) {
-        let s = 512;
-        let sum = 0;
-        let h = this.thread.x;
-        let k = s - 1 - this.thread.y;
-        let index = h * 4 + k * 4 * s;
-        let status = m[index] != 0 ? 1 : 0;
-        for (let j = -1; j <= 1; j++) {
-          for (let i = -1; i <= 1; i++) {
-            let x = (h + i + s) % s;
-            let y = (k + j + s) % s;
-            sum += m[x * 4 + y * 4 * s] != 0 ? 1 : 0;
+    let particles = gpu.createKernel(
+      function () {
+        const width = this.constants.width as number;
+        const height = this.constants.height as number;
+
+        let x = Math.random() * width;
+        let y = Math.random() * height;
+
+        let vx = Math.random() * 2 - 1;
+        let vy = Math.random() * 2 - 1;
+
+        return [x, y, vx * 2, vy * 2];
+      },
+      { output: [particleCount], constants: { width, height } }
+    );
+
+    let stepParticles = gpu.createKernel(
+      function (particles: any) {
+        const width = this.constants.width as number;
+        const height = this.constants.height as number;
+        const particleCount = this.constants.particleCount as number;
+        const g = 1;
+
+        let [x, y, vx, vy] = particles[this.thread.x];
+
+        let fx = 0;
+        let fy = 0;
+
+        for (let i = 0; i < particleCount; i++) {
+          let [x2, y2, vx2, vy2] = particles[i];
+
+          let dx = x - x2;
+          let dy = y - y2;
+          let d = Math.sqrt(dx * dx + dy * dy);
+          if (d > 0 && d < 80) {
+            let f = (g * 1) / d;
+
+            fx += f * dx * 0.5;
+            fy += f * dy * 0.5;
           }
         }
-        sum -= status;
-        let val = 0;
-        if (status == 1 && (sum == 3 || sum == 2)) val = 1;
-        if (status == 1 && (sum < 2 || sum > 3)) val = 0;
-        if (status == 0 && sum == 3) val = 1;
-        this.color(val, val, val);
+
+        x += fx;
+        y += fy;
+
+        x = x % width;
+        y = y % height;
+
+        return [x, y, vx, vy];
       },
-      { output: [dim, dim], graphical: true }
+      { output: [particleCount], constants: { width, height, particleCount } }
     );
 
-    let getInitialBuffer = gpu.createKernel(
-      function () {
-        let val = Math.trunc(Math.random() * 2);
-        this.color(val, val, val);
-      },
-      { output: [dim, dim], graphical: true }
-    );
-    getInitialBuffer();
-    let buffer = getInitialBuffer.getPixels();
+    let renderParticles = gpu.createKernel(
+      function (particles: any) {
+        const particleCount = this.constants.particleCount as number;
 
+        let sum = 0;
+        for (let i = 0; i < particleCount; i++) {
+          let [x, y, vx, vy] = particles[i];
+
+          let dx = x - this.thread.x;
+          let dy = y - this.thread.y;
+
+          let d = Math.pow(Math.sqrt(dx * dx + dy * dy), 2);
+          let v = 2; //Math.max(Math.sqrt(vx * vx + vy * vy), 1);
+
+          sum += v / d;
+        }
+
+        sum *= 0.1;
+
+        this.color(sum, sum, sum, 1);
+      },
+      { output: [width, height], constants: { particleCount }, graphical: true }
+    );
+
+    let buffer = particles();
     let destroyed = false;
 
     const tick = () => {
       if (destroyed) return;
-      runSimulation(buffer);
-      buffer = runSimulation.getPixels();
+      buffer = stepParticles(buffer) as any;
+      renderParticles(buffer);
       requestAnimationFrame(tick);
     };
     requestAnimationFrame(tick);
@@ -63,14 +108,17 @@ const useGPU = (canvas: HTMLCanvasElement | null) => {
 };
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useGPU(canvasRef.current);
+  const [canvasElement, setCanvasElement] = useState<HTMLCanvasElement | null>(
+    null
+  );
+  useGPU(canvasElement);
   return (
     <canvas
-      ref={canvasRef}
+      ref={(e) => {
+        setCanvasElement(e);
+      }}
       style={{
         flex: 1,
-        backgroundColor: "red",
         width: "100vw",
         height: "100vh",
       }}
